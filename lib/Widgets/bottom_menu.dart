2bbf2633a2984fc0e5738847513fcb484568db81
JuEnPeHa <j.enrique.23121996@gmail.com>
2021-12-12 12:44:22 -0600

    update 02

--- lib/Widgets/bottom_menu.dart
+++ lib/Widgets/bottom_menu.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:social_network_test/Widgets/constants.dart';
-import 'package:social_network_test/pages/principal_page.dart';
 
 class BottomMenuLA extends StatefulWidget {
-
   @override
   _BottomMenuLA createState() => _BottomMenuLA();
 }
@@ -16,14 +12,12 @@ class _BottomMenuLA extends State<BottomMenuLA> {
 
   @override
   Widget build(BuildContext context) {
-
-
-return Scaffold(
-  appBar: AppBar(
-    title: Text("Curved Navigation Bar"),
-    elevation: 0,
-    centerTitle: true,
-  ),
-);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Curved Navigation Bar"),
+        elevation: 0,
+        centerTitle: true,
+      ),
+    );
   }
-  }
+}
